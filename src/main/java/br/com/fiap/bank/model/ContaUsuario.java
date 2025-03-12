5642d66ff721e40f95ffbb93e6fc0eb2b6f7e1ab
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-11 21:50:01 -0300

    iniciando a model

---
+++ src/main/java/br/com/fiap/bank/model/ContaUsuario.java
@@ -0,0 +1,17 @@
+package br.com.fiap.bank.model;
+
+import java.time.LocalDate;
+
+public class ContaUsuario {
+
+    private Long numeroConta;
+    private int agencia;
+    private String nomeTitular;
+    private String cpf;
+    private LocalDate dataAbertura;
+    private double saldo;
+    private String ativa;
+    private TipoConta tipo;
+
+
+}
